9da04f07f28ed7a21489d293c47982fc42e8f71a
zombiejuice97 <azan16shaikh@gmail.com>
2021-08-03 17:54:27 +0530

    consecutive ones code

---
+++ Solution.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int[] pairs = new int[8];
+        int consecutives=0;
+
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==1){
+                //if(i<5){
+                if(nums[i]==nums[i+1]){
+                    consecutives++;
+
+                    pairs[i]=consecutives;
+                }
+                //consecutives = 0;
+                //}
+            }
+        }
+
+
+        //return counter;
+        int max = 0;
+
+        for(int j=0;j<pairs.length;j++){
+            if(pairs[j]>max){
+                max = pairs[j];
+
+            }
+        }
+               /*  if ( max%2==0){
+                     max =max-1;
+                 }
+                else{
+                    max = max+1;
+                     }
+*/
+        return max;
+
+
+    }
+}
