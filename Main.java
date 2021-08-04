6a7398e117cc1add001e705d0b99ae485c6ea566
zombiejuice97 <azan16shaikh@gmail.com>
2021-08-04 10:07:48 +0530

    correct code

--- Main.java
+++ Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        int[] arr;
+        arr = new int[]{1, 1, 0, 1, 1,0};
+        Solution trial = new Solution();
+       System.out.println(trial.findMaxConsecutiveOnes(arr));
+      // System.out.println(trial.pairArray());
+    }
 }
--- Solution.java
+++ Solution.java
@@ -1,41 +1,21 @@
 class Solution {
+    int[] totalPairs = new int[8];
+
     public int findMaxConsecutiveOnes(int[] nums) {
-        int[] pairs = new int[8];
-        int consecutives=0;
-
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]==1){
-                //if(i<5){
-                if(nums[i]==nums[i+1]){
-                    consecutives++;
-
-                    pairs[i]=consecutives;
-                }
-                //consecutives = 0;
-                //}
-            }
-        }
-
-
-        //return counter;
         int max = 0;
+        int count = 0;
 
-        for(int j=0;j<pairs.length;j++){
-            if(pairs[j]>max){
-                max = pairs[j];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                    count++;
+                    max = Math.max(count,max);
 
             }
+            else{
+                count=0;
+            }
         }
-               /*  if ( max%2==0){
-                     max =max-1;
-                 }
-                else{
-                    max = max+1;
-                     }
-*/
-        return max;
 
-
+    return max;
     }
-}
-//trash
+    }
